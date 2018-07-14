649b7c654375af5de1f4cb42b3fa8d74ad129c7b
Hayden Betts <haydenbetts@gmail.com>
2018-07-14 17:54:34 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,6 +2,9 @@ class Ship
   attr_accessor :name, :type, :booty
 
   def initialize(attributes)
+    self.name = attributes["name"]
+    self.type = attributes["type"]
+    self.booty = attributes ["booty"]
   end
 
 end

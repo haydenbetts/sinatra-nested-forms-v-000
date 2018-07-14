fc5155a39b9d5bc8cdea44bee0cfe93d02223298
Hayden Betts <haydenbetts@gmail.com>
2018-07-14 18:10:58 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -6,8 +6,12 @@ class Pirate
   def self.all
     @@all
   end
-  
+
   def initialize(params)
+    # initialize pirates using only the
+    params.each do |key, value|
+      self.send("#{key}=", value)
+    end
     name = params["name"]
     weight = params["weight"]
     height = params["height"]

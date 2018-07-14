c6557476ebb8e653a41aa6adf5fbefab7a7c85b7
Hayden Betts <haydenbetts@gmail.com>
2018-07-14 17:54:00 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,9 +2,11 @@ class Pirate
   attr_accessor :name, :weight, :height
 
   @@all
+
   def self.all
     @@all
   end
+  
   def initialize(params)
     name = params["name"]
     weight = params["weight"]
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,4 @@
 class Ship
+  attr_accessor :name, :type, :boot
+
 end
